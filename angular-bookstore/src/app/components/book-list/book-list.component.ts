7017c926ee78269f5ca42dffd74a9dcd1981ed82
Touheed KHAN <TOUHEED.KHAN@SAINT-GOBAIN.COM>
2020-06-07 23:34:42 +0530

    feature 1: Angular Project created

---
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from 'src/app/common/book';
+import { BookService } from 'src/app/services/book.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css'],
+})
+export class BookListComponent implements OnInit {
+  constructor(private bookService: BookService) {}
+  books: Book[];
+  ngOnInit(): void {
+    this.listBooks();
+  }
+  listBooks() {
+    this.bookService.getBooks().subscribe((data) => {
+      this.books = data;
+    });
+  }
+}
--- springboot_bookstore/src/main/java/com/touheedkhan/springboot_bookstore/repository/BookRepository.java
+++ springboot_bookstore/src/main/java/com/touheedkhan/springboot_bookstore/repository/BookRepository.java
@@ -3,7 +3,9 @@ package com.touheedkhan.springboot_bookstore.repository;
 import com.touheedkhan.springboot_bookstore.entity.Book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
